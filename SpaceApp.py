c25c9a0ad71ba69bc5aa9080364c7c4b05f80a3a
shoo <azshoo@gmail.com>
2017-04-29 16:19:33 +0300

    Added current monitoring data + route for this.

--- SpaceApp.py
+++ SpaceApp.py
@@ -31,6 +31,7 @@ def get_panel_status(panel_id):
 def get_panel_datas():
     return '{datas:[]}'
 
+
 @app.route('/panels/data/<int:panel_id>', methods=['GET'])
 def get_panel_data(panel_id):
     return '{data:{%d}}' % panel_id
@@ -49,7 +50,38 @@ def add_panel():
         return "error"
 
 
+#TODO Add sunrise\sunset and Cloudiness
+# and kill my self for this code.
+def get_monitoring_data(timestamp):
+    sql = """SELECT  `nasa_humidity`.`value` AS humidity,
+            `nasa_barometric_pressure`.`value` AS barometric_pressure,
+            `nasa_solar_radiation`.`value` AS solar_radiation,
+            `nasa_temperature`.`value` AS temperature,
+            `nasa_wind_direction`.`value` AS wind_direction,
+            `nasa_wind_speed`.`value` AS wind_speed
+    FROM nasa_humidity
+    INNER JOIN nasa_barometric_pressure ON nasa_humidity.timestamp=nasa_barometric_pressure.timestamp
+    INNER JOIN nasa_solar_radiation ON nasa_humidity.timestamp=nasa_solar_radiation.timestamp
+    INNER JOIN nasa_temperature ON nasa_humidity.timestamp=nasa_temperature.timestamp
+    INNER JOIN nasa_wind_direction ON nasa_humidity.timestamp=nasa_wind_direction.timestamp
+    INNER JOIN nasa_wind_speed ON nasa_humidity.timestamp=nasa_wind_speed.timestamp
+    WHERE `nasa_humidity`.timestamp=%d""" % timestamp
+    cursor.execute(sql)
+    values = cursor.fetchall()
+    data = {'humidity': float(values[0][0]), 'barometric_pressure': float(values[0][1]),
+            'solar_radiation': float(values[0][2]), 'temperature': float(values[0][3]),
+            'wind_direction': float(values[0][4]), 'wind_speed': float(values[0][5])}
+    res = json.dumps(data)
+    return res
+
+
 # TODO get current weather (Temperature | Pressure | Humidity | Solar radiation | Cloudiness | Sunrise / sunset | Wind speed | wind direction)
+@app.route('/monitoring/current', methods=['GET'])
+def get_current_monitoring_data():
+    last_timestamp_exists = 1475243723
+    res = get_monitoring_data(last_timestamp_exists)
+    return res
+
 @app.route('/panel/weather', methods=['GET'])
 def get_weather():
     # sql = "SELECT * FROM nasa_db.nasa_humidity WHERE `timestamp` < 1473010221"
@@ -59,8 +91,9 @@ def get_weather():
     # return res
     error = None
     if request.method == 'GET':
-        name = request.form['timestamp']
-        sql = "SELECT * FROM nasa_db.nasa_humidity WHERE `timestamp` < %d" % name
+        # name = request.form['timestamp']
+        current_timestamp = 1475243723
+        sql = "SELECT * FROM nasa_db.nasa_humidity WHERE `timestamp` = %d" % current_timestamp
         cursor.execute(sql)
         data = cursor.fetchall()
         res = json.dumps(data)
@@ -71,5 +104,7 @@ def get_weather():
 
 # TODO get today sun status
 
+
+
 if __name__ == '__main__':
-    app.run(host='95.46.99.185')
+    app.run(host='127.0.0.1')
