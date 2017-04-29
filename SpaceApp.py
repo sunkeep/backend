2db4d16f6b6d046436d7528a4fd17d1df1a77023
Vasiliy Saenko <saenko@mqd.me>
2017-04-29 10:26:51 +0300

    test get weather request

--- SpaceApp.py
+++ SpaceApp.py
@@ -49,5 +49,27 @@ def add_panel():
         return "error"
 
 
+# TODO get current weather (Temperature | Pressure | Humidity | Solar radiation | Cloudiness | Sunrise / sunset | Wind speed | wind direction)
+@app.route('/panel/weather', methods=['GET'])
+def get_weather():
+    # sql = "SELECT * FROM nasa_db.nasa_humidity WHERE `timestamp` < 1473010221"
+    # cursor.execute(sql)
+    # data = cursor.fetchall()
+    # res = json.dumps(data)
+    # return res
+    error = None
+    if request.method == 'GET':
+        name = request.form['timestamp']
+        sql = "SELECT * FROM nasa_db.nasa_humidity WHERE `timestamp` < %d" % name
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        res = json.dumps(data)
+        return res
+    else:
+        return "error"
+
+
+# TODO get today sun status
+
 if __name__ == '__main__':
     app.run(host='95.46.99.185')
